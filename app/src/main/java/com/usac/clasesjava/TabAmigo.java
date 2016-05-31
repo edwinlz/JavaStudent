20411ac36013fe75a2682c7c116b77d87f88f8f5
Felipe Lopez de Leon <felipeantonio.0804@gmail.com>
2016-05-30 23:48:38 -0600

    Cambios en el registro de GetAmistades y eliminarAmistades

--- app/src/main/java/com/usac/clasesjava/TabAmigo.java
+++ app/src/main/java/com/usac/clasesjava/TabAmigo.java
@@ -2,10 +2,14 @@ package com.usac.clasesjava;
 
 import android.app.ActionBar;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,14 +24,19 @@ import android.widget.Toast;
 import com.usac.javastudent.Amigos;
 import com.usac.javastudent.R;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Edwin on 30/05/2016.
  */
 public class TabAmigo extends Fragment {
-
     private ViewGroup layout;
 
     @Override
@@ -42,17 +51,45 @@ public class TabAmigo extends Fragment {
                 Intent m = new Intent(getActivity(),Amigos.class);
                 m.putExtras(b);
                 startActivity(m);
-
             }
         });
 
         layout = (ViewGroup)aux.findViewById(R.id.contentFA);
-        pintarAmigos();
-
+        new CallGetAmistades().execute();
         return  aux;
     }
 
+    private void procesarRespuesta(String respuesta) {
+        String spliteo[] = respuesta.split("!");
+        String datos[] = new String[spliteo.length-3];
+        for (int i=3;i<spliteo.length;i++){
+            datos[i-3] = spliteo[i];
+        }
 
+        ArrayList<Amigo> amigos = new ArrayList<Amigo>();
+        Amigo amigo = null;
+        for(int i=0;i<datos.length;i++){
+            if(i%3==0){
+                amigo = new Amigo();
+                amigo.setUsername(datos[i]);
+            }
+            else if(i%3==1){
+                amigo.setExperiencia(Integer.parseInt(datos[i]));
+            }
+            else{
+                amigo.setAvatar(Integer.parseInt(datos[i]));
+                amigos.add(amigo);
+                amigo = null;
+            }
+        }
+        pintarAmigos(amigos);
+    }
+
+    private void pintarAmigos(ArrayList<Amigo> amigos){
+        for(Amigo mod:amigos){
+            setAmigo(mod);
+        }
+    }
 
     public void setAmigo(Amigo am){
         LayoutInflater inflater = LayoutInflater.from(getActivity());
@@ -90,8 +127,28 @@ public class TabAmigo extends Fragment {
         layout.addView(relativeLayout);
     }
 
-    public void preguntar(final String user){
+    public int getIdImage(int imagen){
+        switch (imagen){
+            case 1: return R.drawable.user_boy_3;
+            case 2: return R.drawable.user_man_1;
+            default: return R.drawable.user_boy_3;
+        }
+    }
 
+
+
+    private void respuestaSolicitudAmistad(String respuesta) {
+        Toast toast = Toast.makeText(getContext(), respuesta, Toast.LENGTH_LONG);
+        toast.show();
+        new CallGetAmistades().execute();
+    }
+
+    public void elimarAmigo(String username){
+        layout.removeAllViews();
+        new CallEliminarAmistad(username).execute();
+    }
+
+    public void preguntar(final String user){
         AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
         alert.setTitle("¿Desea eliminar a "+user + " de sus amigos?");
 
@@ -110,48 +167,147 @@ public class TabAmigo extends Fragment {
         alert.show();
     }
 
-    public void elimarAmigo(String username){
-        layout.removeAllViews();
-        repintarAmigos();
-        Toast.makeText(getActivity(), "Se ha eliminado", Toast.LENGTH_LONG).show();
+
+    private static final String NAMESPACE = "http://tempuri.org/";
+    private static final String URL="http://felipeantonio.somee.com/ServicioJavaStudent.asmx?WSDL";
+    private static final String METHOD_NAME = "getAmistades";
+    private static final String SOAP_ACTION ="http://tempuri.org/getAmistades";
+
+    private static final String NAMESPACE2 = "http://tempuri.org/";
+    private static final String URL2="http://felipeantonio.somee.com/ServicioJavaStudent.asmx?WSDL";
+    private static final String METHOD_NAME2 = "eliminarAmistad";
+    private static final String SOAP_ACTION2 ="http://tempuri.org/eliminarAmistad";
+    private SoapObject request=null;
+    private SoapSerializationEnvelope envelope=null;
+    private SoapPrimitive resultsRequestSOAP=null;
+    private String respuesta;
+    private ProgressDialog dialogo;
+
+    public Boolean invocarWSGetAmistades() {
+        Boolean bandera = true;
+        try{
+            request = new SoapObject(NAMESPACE, METHOD_NAME);
+            request.addProperty("username",Estatica.username_actual);
+
+            envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = true;
+            envelope.setOutputSoapObject(request);
+            HttpTransportSE transporte = new HttpTransportSE(URL);
+            transporte.call(SOAP_ACTION, envelope);
+            resultsRequestSOAP = (SoapPrimitive) envelope.getResponse();
+            respuesta = resultsRequestSOAP.toString();
+            Log.e("Valor del response: ", resultsRequestSOAP.toString());
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Log.v("Error", "Error io: "+e.toString());
+            bandera= false;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Log.v("Error", "Error exception: "+e.toString());
+            bandera = false;
+        }
+        return bandera;
     }
 
-    public int getIdImage(int imagen){
-        switch (imagen){
-            case 1: return R.drawable.user_boy_3;
-            case 2: return R.drawable.user_man_1;
-            default: return R.drawable.user_boy_3;
+    public Boolean invocarWSEliminarAmistad(String usernameDueno, String usernameAmigo) {
+        Boolean bandera = true;
+        try{
+            request = new SoapObject(NAMESPACE2, METHOD_NAME2);
+            request.addProperty("usernameDueno",usernameDueno);
+            request.addProperty("usernameAmigo",usernameAmigo);
+            envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = true;
+            envelope.setOutputSoapObject(request);
+            HttpTransportSE transporte = new HttpTransportSE(URL2);
+            transporte.call(SOAP_ACTION2, envelope);
+            resultsRequestSOAP = (SoapPrimitive) envelope.getResponse();
+            respuesta = resultsRequestSOAP.toString();
+            Log.e("Valor del response: ", resultsRequestSOAP.toString());
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Log.v("Error", "Error io: "+e.toString());
+            bandera= false;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Log.v("Error", "Error exception: "+e.toString());
+            bandera = false;
+        }
+        return bandera;
+    }
+
+    class CallGetAmistades extends AsyncTask<String,String,String> {
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            dialogo = new ProgressDialog(getContext());
+            dialogo.setMessage("Procesando solicitud...");
+            dialogo.setIndeterminate(false);
+            dialogo.setCancelable(false);
+            dialogo.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(invocarWSGetAmistades()){
+                return "ok";
+            }
+            else{
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //super.onPostExecute(s);
+            dialogo.dismiss();
+            if(result.contains("ok")){
+                procesarRespuesta(respuesta);
+            }
         }
     }
 
-    private void repintarAmigos(){
-        List<Amigo> lstAmigos = getAmigosLocal2();
-        for(Amigo mod:lstAmigos){
-            setAmigo(mod);
+    class CallEliminarAmistad extends AsyncTask<String,String,String> {
+        private String usernameAmistad;
+
+        public CallEliminarAmistad(String usernameAmistad){
+            this.usernameAmistad = usernameAmistad;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            dialogo = new ProgressDialog(getContext());
+            dialogo.setMessage("Procesando solicitud...");
+            dialogo.setIndeterminate(false);
+            dialogo.setCancelable(false);
+            dialogo.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(invocarWSEliminarAmistad(Estatica.username_actual,usernameAmistad)){
+                return "ok";
+            }
+            else{
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //super.onPostExecute(s);
+            dialogo.dismiss();
+            if(result.contains("ok")){
+                respuestaSolicitudAmistad(respuesta);
+            }
         }
     }
 
-    private void pintarAmigos(){
-        List<Amigo> lstAmigos = getAmigosLocal();
-        for(Amigo mod:lstAmigos){
-            setAmigo(mod);
-        }
-    }
-
-    public List<Amigo> getAmigosLocal(){
-        ArrayList<Amigo> amigo_ = new ArrayList<Amigo>();
-        amigo_.add(new Amigo("Edwin", 100,1));
-        amigo_.add(new Amigo("Raul", 1000,2));
-        amigo_.add(new Amigo("Rony", 10000,1));
-        amigo_.add(new Amigo("Crazy", 100000,2));
-        return amigo_;
-    }
-
-    public List<Amigo> getAmigosLocal2(){
-        ArrayList<Amigo> amigo_ = new ArrayList<Amigo>();
-        amigo_.add(new Amigo("Edwin", 100,1));
-        amigo_.add(new Amigo("Raul", 1000,2));
-        return amigo_;
-    }
 
 }
